e228c5168ea7a50c393af5e0050d2e96064a71af
bahram atahanov <bahram101@AuraITs-MacBook-Pro.local>
2025-09-17 23:15:31 +0500

    add tabs

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -2,8 +2,6 @@ import { Stack } from "expo-router";
 
 export default function MainLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Главная" }} />
-    </Stack>
+    <Stack screenOptions={{headerShown: false}}/>
   );
 }
