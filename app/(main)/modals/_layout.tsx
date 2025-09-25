870b591bd5cccb8bdc668c50c4d3889bfa928aa3
Bahrom <bahram8787@gmail.com>
2025-09-25 12:06:10 +0500

    add CloseButton to Tab

--- app/(main)/modals/_layout.tsx
+++ app/(main)/modals/_layout.tsx
@@ -1,54 +1,39 @@
 import { Stack } from "expo-router";
-import { Platform } from "react-native";
-
-// export default function MainLayout() {
-//   return (
-//     <Stack screenOptions={{ headerShown: false }}>
-//       <Stack.Screen
-//         name="modal"
-//         options={{
-//           presentation: "transparentModal", // fullScreenModal, modal, transparentModal
-//           animation: Platform.OS === "ios" ? "slide_from_bottom" : "fade", //fade, slide_from_right
-//           headerShown: false,
-//         }}
-//       />
-//     </Stack>
-//   );
-// }
 
 export default function MainModalsLayout() {
-  const modalOpts = {
-    presentation: "modal" as const, // или "transparentModal"/"fullScreenModal"
-    animation: Platform.OS === "ios" ? "slide_from_bottom" : "slide_from_right",
-    headerShown: false,
-  };
+  // const modalOpts = {
+  //   presentation: "modal" as const, // или "transparentModal"/"fullScreenModal"
+  //   animation: Platform.OS === "ios" ? "slide_from_bottom" : "slide_from_right",
+  //   headerShown: false,
+  // };
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen
         name="service"
         options={{
-          presentation: "modal" as const,
-          animation:
-            Platform.OS === "ios" ? "slide_from_bottom" : "slide_from_right",
+          presentation: "transparentModal" as const,
+          // animation:
+          //   Platform.OS === "ios" ? "slide_from_bottom" : "slide_from_right",
           headerShown: false,
         }}
       />
       <Stack.Screen
         name="finance"
         options={{
-          presentation: "modal" as const,
-          animation:
-            Platform.OS === "ios" ? "slide_from_bottom" : "slide_from_right",
+          presentation: "modal",
+          animation: 'fade', 
+          // animation:
+          //   Platform.OS === "ios" ? "slide_from_bottom" : "slide_from_right",
           headerShown: false,
         }}
       />
       <Stack.Screen
         name="marketing"
         options={{
-          presentation: "modal" as const,
-          animation:
-            Platform.OS === "ios" ? "slide_from_bottom" : "slide_from_right",
+          presentation: "modal",
+          // animation:
+          //   Platform.OS === "ios" ? "slide_from_bottom" : "slide_from_right",
           headerShown: false,
         }}
       />
--- app/(main)/modals/service/_layout.tsx
+++ app/(main)/modals/service/_layout.tsx
@@ -1,12 +1,23 @@
 // app/(main)/modals/service/_layout.tsx
+import CloseButton from "@/components/ui/button/CloseButton";
 import { Tabs } from "expo-router";
 
 export default function ServiceTabsLayout() {
   return (
-    <Tabs initialRouteName="tasks" screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="tasks"    options={{ title: "Задачи" }} />
+    <Tabs
+      initialRouteName="tasks"
+      screenOptions={{
+        headerShown: true,
+        headerRight: () => <CloseButton />,
+       
+        tabBarActiveTintColor: "orange",
+        tabBarInactiveTintColor: "rgba(255,255,255,0.7)",
+        // sceneContainerStyle: { backgroundColor: "#0b1220" },
+      }}
+    >
+      <Tabs.Screen name="tasks" options={{ title: "Задачи" }} />
       <Tabs.Screen name="messages" options={{ title: "Сообщения" }} />
-      <Tabs.Screen name="news"     options={{ title: "Новости" }} />
+      <Tabs.Screen name="news" options={{ title: "Новости" }} />
     </Tabs>
   );
 }
