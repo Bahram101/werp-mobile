47688ea0f1a40599848cf6341c76bde88a057d90
Bahrom <bahram8787@gmail.com>
2025-11-12 23:29:41 +0500

    start bottomSheet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,16 +4,19 @@ import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import AuthProvider from "@/features/auth/components/AuthProvider";
 import "@/global.css";
 import { ReactQueryProvider } from "@/providers/ReactQueryProvider";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import RootLayout from "./RootLayout";
 
 export default function RootlayoutWrapper() {
   return (
-    <ReactQueryProvider>
-      <AuthProvider>
-        <GluestackUIProvider mode="light">
-          <RootLayout />
-        </GluestackUIProvider>
-      </AuthProvider>
-    </ReactQueryProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ReactQueryProvider>
+        <AuthProvider>
+          <GluestackUIProvider mode="light">
+            <RootLayout />
+          </GluestackUIProvider>
+        </AuthProvider>
+      </ReactQueryProvider>
+    </GestureHandlerRootView>
   );
 }
