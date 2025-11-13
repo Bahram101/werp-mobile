c4c2ec489d42b216b9c584b4f7c6364e1f18f4eb
Bahrom <bahram8787@gmail.com>
2025-11-13 21:05:28 +0500

    remove styleSheet

--- app/(master)/(tabs)/profile.tsx
+++ app/(master)/(tabs)/profile.tsx
@@ -1,6 +1,6 @@
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import React, { useCallback, useRef } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 const App = () => {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -12,24 +12,12 @@ const App = () => {
   return (
     <View className="flex-1 items-center justify-center bg-orange-200">
       <BottomSheet ref={bottomSheetRef} onChange={handleSheetChanges}>
-        <BottomSheetView >
+        <BottomSheetView className="p-10">
           <Text>Awesome 🎉</Text>
         </BottomSheetView>
       </BottomSheet>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "grey",
-  },
-  contentContainer: {
-    flex: 1,
-    padding: 36,
-    alignItems: "center",
-  },
-});
 
 export default App;
