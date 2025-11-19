82ceef9bd66cc2fba5ea2a22a58115064cd177ae
Bahrom <bahram8787@gmail.com>
2025-11-19 18:15:23 +0500

    start master work form

--- features/master/requests/screens/RequestDetail/RequestWork/index.tsx
+++ features/master/requests/screens/RequestDetail/RequestWork/index.tsx
@@ -1,11 +1,3 @@
-import {
-  Table,
-  TableBody,
-  TableData,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import React, { useEffect } from "react";
 import { Text, View } from "react-native";
@@ -22,45 +14,58 @@ const RequestWorkScreen = () => {
     }
   }, [navigation, number]);
 
-  console.log("ID=>", id);
-  console.log("NUMBER=>", number);
   const data = [
     { id: 1, name: "Установка", price: 7500, currency: "KZT" },
     { id: 2, name: "Услуга", price: 2500, currency: "KZT" },
+    { id: 3, name: "Монтаж", price: 1500, currency: "KZT" },
   ];
+
+  const totalPrice = data.reduce((acc, item) => acc + item.price, 0);
 
   return (
     <View className="bg-white mt-3 rounded-2xl p-3 mx-4 ">
-      <View className="flex-row items-center pb-4 pt-3 border-b mb-4 border-grayLight gap-2">
+      <View className="flex-row items-center pb-4 pt-2 border-b mb-4 border-grayLight gap-2">
         <Text className="font-bold text-primary">УСЛУГИ</Text>
       </View>
-      <Table className="w-full">
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[30px]">№</TableHead>
-            <TableHead className="text-sm">Название усл</TableHead>
-            <TableHead>Сумма</TableHead>
-            <TableHead>Валюта</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {data.map((item, index) => (
-            <TableRow key={item.id}>
-              <TableData>{item.id}</TableData>
-              <TableData>{item.name}</TableData>
-              <TableData>{item.price}</TableData>
-              <TableData>{item.currency}</TableData>
-            </TableRow>
-          ))}
-        </TableBody>
-        {/* <TableFooter>
-          <TableRow>
-            <TableHead>Total</TableHead>
-            <TableHead>22</TableHead>
-            <TableHead>$340</TableHead>
-          </TableRow>
-        </TableFooter> */}
-      </Table>
+      <View>
+        <View className="table">
+          <View className="table-header flex-row justify-between border-b border-grayLight pb-2">
+            <View className="table-head-col w-[10%]">
+              <Text className="font-semibold">№</Text>
+            </View>
+            <View className="table-head-col w-[50%]">
+              <Text className="font-semibold">Название усл</Text>
+            </View>
+            <View className="table-head-col w-[20%]">
+              <Text className="font-semibold">Сумма</Text>
+            </View>
+            <View className="table-head-col w-[20%] border-r border-grayLight">
+              <Text className="font-semibold">Валюта</Text>
+            </View>
+          </View>
+          <View className="table-body flex-col">
+            {data.map((item) => (
+              <View key={item.id} className="flex-row justify-between border-b border-grayLight py-2">
+                <View className="justify-center flex-row w-[10%]">
+                  <Text>{item.id}</Text>
+                </View>
+                <View className="justify-center flex-row w-[50%]">
+                  <Text>{item.name}</Text>
+                </View>
+                <View className="justify-center flex-row w-[20%]">
+                  <Text>{item.price}</Text>
+                </View>
+                <View className="justify-center flex-row w-[20%]">
+                  <Text>{item.currency}</Text>
+                </View>
+              </View>
+            ))}
+          </View>
+          <View className="flex-row justify-end pt-3">
+            <Text>{`Итог: ${totalPrice}`}</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
