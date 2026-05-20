474eea8ed303c80e86f1af7a115eabdd9f381444
Bahrom <bahram8787@gmail.com>
2026-05-20 16:37:23 +0500

    add app icon

--- services/api/config.ts
+++ services/api/config.ts
@@ -3,10 +3,4 @@ import Constants from "expo-constants";
 export const SERVER_URL = Constants.expoConfig?.extra?.SERVER_URL;
 export const API_URL = `${SERVER_URL}/api`;
 
-// export const AUTH_URL = `${SERVER_URL}:${process.env.EXPO_PUBLIC_PORT_AUTH}`;
-// export const CORE_URL = `${SERVER_URL}:${process.env.EXPO_PUBLIC_PORT_CORE}`;
-// export const SERVICE_URL = `${SERVER_URL}:${process.env.EXPO_PUBLIC_PORT_SERVICE}`;
-// export const CALL_CENTER_URL = `${SERVER_URL}:${process.env.EXPO_PUBLIC_PORT_CALL_CENTER}`;
-// export const CRM_URL = `${SERVER_URL}:${process.env.EXPO_PUBLIC_PORT_CRM}`;
-
 console.log("SERVER_URL", SERVER_URL);
