9bec7a22cbf984d27bc0694bd13a00bfb9f6a290
Bahrom <bahram8787@gmail.com>
2026-06-12 19:28:10 +0500

    1.0.8

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "merp",
     slug: "merp",
-    version: "1.0.7",
+    version: "1.0.8",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "merp",
